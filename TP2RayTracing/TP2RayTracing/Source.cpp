0239b4e1b2207777741470c0d21b116abac7116a
mlivebardo <tsunoll96@gmail.com>
2020-09-15 17:27:23 +0200

    Lampe

--- TP2RayTracing/TP2RayTracing/Source.cpp
+++ TP2RayTracing/TP2RayTracing/Source.cpp
@@ -25,10 +25,9 @@ optional<float> raySphereIntersect(Rayon r, Sphere s) {
     float b = 2.0 * r.GetDirection().dot(s0_r0);
     float c = s0_r0.dot(s0_r0) - (s.GetRadius() * s.GetRadius());
     float delta = b * b - 4.0 * a * c;
-    if ( delta < 0.0) {
+    if (delta < 0.0) {
         return std::nullopt;
     }
-    
     float resultat = (-b - sqrt(delta)) / (2.0 * a);
     if (resultat >= 0)
     {
@@ -55,8 +54,8 @@ int main(int argc, char* argv[])
     //NOTE: this sample will overwrite the file or test.png without warning!
     const char* filename = argc > 1 ? argv[1] : "test.png";
     vector<Sphere> spheres;   
-    spheres.push_back(Sphere(100.0f, Point(200, 300, 500)));
-    spheres.push_back(Sphere(110.0f, Point(200, 100, 400)));
+    spheres.push_back(Sphere(100.0f, Point(100, 350, 400)));
+    spheres.push_back(Sphere(100.0f, Point(100, 100, 400)));
 
     //generate some image
     unsigned width = 512, height = 512;
@@ -66,6 +65,7 @@ int main(int argc, char* argv[])
         for (unsigned x = 0; x < width; x++) {
             Rayon r(Point((float)x, (float)y, 0), Direction(0, 0, 1));
             optional<float>min_dst = nullopt;
+            //optional<>
             for (int i = 0; i < spheres.size(); i++)
             {
                 auto dst = raySphereIntersect(r, spheres[i]);
@@ -85,14 +85,28 @@ int main(int argc, char* argv[])
                         {
                             color(image, Vector3(x, y, 0), width, 255, 255, 255, 255);
                         }
+
                     }
                 }
                 else if (!min_dst)
                 {
-                    color(image, Vector3(x, y, 0), width,0, 0, 0, 255);
+                    color(image, Vector3(x, y, 0), width,0, 0, 100, 255);
                 }
-
             }
+
+            if (min_dst)
+            {
+                for (size_t i = 0; i < spheres.size(); i++)
+                {
+                    auto lampe = raySphereIntersect(Rayon(Point((float)x, (float)y, min_dst.value() + 0.02f), Direction(0,1,0)), spheres[i]);
+                    if (lampe)
+                    {
+                        color(image, Vector3(x, y, 0), width, 0, 0, 0, 255);
+                    }
+                }
+            }
+            
+
         }
 
     encodeOneStep(filename, image, width, height);
