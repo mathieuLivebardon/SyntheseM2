a4623ec4aa4cc41c1718c373d1f0c3e96ea72c22
mlivebardo <tsunoll96@gmail.com>
2020-09-29 17:09:10 +0200

    Epsilone WIP

--- TP2RayTracing/TP2RayTracing/Source.cpp
+++ TP2RayTracing/TP2RayTracing/Source.cpp
@@ -74,7 +74,26 @@ Color computeColor(Lampe lampe, Sphere S, Vector3 A,Point X)
     return Color(col.x, col.y, col.z);
 }
 
-void lancerRayon(Rayon r, vector<Lampe> lampes, vector<Sphere> spheres, vector<unsigned char>& image, unsigned width)
+void searchCloserObject(vector<Sphere> spheres,Rayon r, int &ispheres, optional<float> &min_dst)
+{
+    for (int i = 0; i < spheres.size(); i++)
+    {
+        auto dst = raySphereIntersect(r, spheres[i]);
+        if (dst)
+        {
+
+            if (!min_dst || min_dst.value() > dst.value())
+            {
+                min_dst = dst;
+                ispheres = i;
+            }
+        }
+    }
+
+}
+
+
+void lancerRayon(Rayon r, vector<Lampe> lampes, vector<Sphere> spheres, vector<unsigned char>& image, unsigned width,Vector3 pixel)
 {
     for (int l = 0; l < lampes.size(); l++)
     {
@@ -84,67 +103,57 @@ void lancerRayon(Rayon r, vector<Lampe> lampes, vector<Sphere> spheres, vector<u
         int sphereindex = 0;
         optional<float>lampe = nullopt;
         optional<float>min_dst = nullopt;
-        for (int i = 0; i < spheres.size(); i++)
-        {
-            auto dst = raySphereIntersect(r, spheres[i]);
-            if (dst && !spheres[i].Mirror())
-            {
-                if (!min_dst || min_dst.value() > dst.value())
-                {
-                    min_dst = dst;
-                    sphereindex = i;
-                    X = Point(r.GetOrigin().x, r.GetOrigin().y, min_dst.value() - 0.02f);
-                    vec_dir = (lampes[l].GetPos() - X.GetPos()).normalize();
-                    dir = Direction(vec_dir.x, vec_dir.y, vec_dir.z);
-                    Vector3 L = X.GetPos() - lampes[l].GetPos();
-                    float d = L.length();
-                    for (int j = 0; j < spheres.size(); j++)
-                    {
-                        auto interlampe = raySphereIntersect(Rayon(X, dir), spheres[j]);
-                        if (interlampe)
-                        {
-                            if (interlampe.value() < d)
-                            {
-                                lampe = interlampe;
-                            }
-                        }
-                    }
-                }
-            }
-            else if(dst && spheres[i].Mirror())
-            {
-                min_dst = dst;
-                Point X(r.GetOrigin().x, r.GetOrigin().y, dst.value() - 0.02f);
-                Vector3 I = r.GetDirection();
-                Vector3 N = X.GetPos() - spheres[i].GetCenter();
-                N = N.normalize();
 
-                Direction R((-I.dot(N) * N * 2 + I).normalize());
+        searchCloserObject(spheres,r,sphereindex, min_dst);
 
-                Rayon r2(X, R);
-                lancerRayon(r2, lampes, spheres, image, width);
-            }
-
-        }
         if (min_dst)
         {
-
-            if (lampe)
-            {
-                color(image, Vector3(r.GetOrigin().x, r.GetOrigin().y, 0), width, 0, 0, 0, 255);
+            float epsilone = -0.1;
+            
+            X = Point(r.GetOrigin().x + min_dst.value() * r.GetDirection().x, r.GetOrigin().y + min_dst.value() * r.GetDirection().y, r.GetOrigin().z + min_dst.value() * r.GetDirection().z + r.GetDirection().z*epsilone);
+            if (!spheres[sphereindex].Mirror())
+            {               
+                vec_dir = (lampes[l].GetPos() - X.GetPos()).normalize();
+                dir = Direction(vec_dir.x, vec_dir.y, vec_dir.z);
+                Vector3 L = X.GetPos() - lampes[l].GetPos();
+                float d = L.length();
+                int sphereCloserLampe = 0 ;
+                searchCloserObject(spheres, Rayon(X, dir), sphereCloserLampe, lampe);
+                if (lampe)
+                {
+                    if (lampe.value() > d)
+                    {
+                        Color c = computeColor(lampes[l], spheres[sphereindex], spheres[sphereindex].GetAlbedo(), X);
+                        color(image, pixel, width, c.GetColorRGB().x, c.GetColorRGB().y, c.GetColorRGB().z, 255);
+                    }
+                    else
+                    {
+                        color(image, pixel, width, 0, 0, 0, 255);
+                    }
+                }
+                else
+                {
+                    Color c = computeColor(lampes[l], spheres[sphereindex], spheres[sphereindex].GetAlbedo(), X);
+                    color(image, pixel, width, c.GetColorRGB().x, c.GetColorRGB().y, c.GetColorRGB().z, 255);
+                }
+                
             }
             else
             {
-                Color c = computeColor(lampes[l], spheres[sphereindex], Vector3(1, 1, 1), X);
-                color(image, Vector3(r.GetOrigin().x, r.GetOrigin().y, 0), width, c.GetColorRGB().x, c.GetColorRGB().y, c.GetColorRGB().z, 255);
+                Vector3 I = r.GetDirection();
+                Vector3 N = X.GetPos() - spheres[sphereindex].GetCenter();
+                N = N.normalize();
+                Direction R((-I.dot(N) * N * 2 + I).normalize());
+
+                Rayon r2(X, R);
+                lancerRayon(r2, lampes, spheres, image, width, pixel);
             }
-        }
+        }  
         else
         {
-            color(image, Vector3(r.GetOrigin().x, r.GetOrigin().y, 0), width, 100, 100, 100, 255);
-        }
+            color(image, pixel, width, 0, 0, 0, 255);
+        }    
     }
-
 }
 
 
@@ -160,66 +169,33 @@ int main(int argc, char* argv[])
     const char* filename = argc > 1 ? argv[1] : "RayTracing.png";
     vector<Sphere> spheres;   
     spheres.push_back(Sphere(100.0f, Point(100, 700, 400),Vector3(1,0,0)));
-    spheres.push_back(Sphere(200.0f, Point(200, 120, 400),Vector3(1,0,1)));
-   // spheres.push_back(Sphere(150.0f, Point(600, 702, 400),Vector3(0, 0, 1)));
-    spheres.push_back(Sphere(100.0f, Point(600, 300, 500),Vector3(1,1,1), true));
-    spheres.push_back(Sphere(1000.0f, Point(500, 1500, 1000),Vector3(0, 1, 1)));
-
-
-    
+    //spheres.push_back(Sphere(200.0f, Point(200, 120, 700),Vector3(1,0,1)));
+    spheres.push_back(Sphere(150.0f, Point(600, 702, 400),Vector3(0, 0, 1)));
+    spheres.push_back(Sphere(100000.0f, Point(500, 101000, 500),Vector3(0, 1, 1)));
+    spheres.push_back(Sphere(100000.0f, Point(101000, 500, 500),Vector3(0, 1, 0)));
+    //spheres.push_back(Sphere(200.0f, Point(600, 300, 700),Vector3(0,0,0), true));
+    spheres.push_back(Sphere(500.0f, Point(0, 0, 700),Vector3(0,0,0), true));
 
     vector<Lampe> lampes;
-    lampes.push_back(Lampe(Point(350, 400, 200), Vector3(60000000, 60000000, 60000000)));
-    lampes.push_back(Lampe(Point(145, 500, 400), Vector3(60000000, 0, 0)));
-    lampes.push_back(Lampe(Point(750, 110, 300), Vector3(0, 0, 60000000)));
+    lampes.push_back(Lampe(Point(350, 400, 0), Vector3(600000000, 600000000, 600000000)));
+    //lampes.push_back(Lampe(Point(700, 0, 200), Vector3(600000000, 0, 0)));
+    //lampes.push_back(Lampe(Point(750, 110, 300), Vector3(0, 0, 600000000)));
     
     //generate some image
     unsigned width = 1000, height = 1000;
     std::vector<unsigned char> image;
     image.resize(width * height * 4);
+    Point Camera(500, 500, -1000);
     for (unsigned y = 0; y < height; y++)
     {
         for (unsigned x = 0; x < width; x++) 
         {
-            lancerRayon(Rayon(Point((float)x, (float)y, 0), Direction(0, 0, 1)),lampes,spheres,image, width);
-            
+            Direction d = (Vector3(x, y, 0) - Camera.GetPos()).normalize();
+            lancerRayon(Rayon(Point((float)x, (float)y, 0), d),lampes,spheres,image, width, Vector3(x,y,0));       
         }
     }
 
         
-        /*for (unsigned y = 0; y < height; y++)
-        {
-            for (unsigned x = 0; x < width; x++) 
-            {
-                Rayon r(Point((float)x, (float)y, 0), Direction(0, 0, 1));
-                for (int i = 0; i < sphereMiror.size(); i++)
-                {
-                    auto dst = raySphereIntersect(r, spheres[i]);
-                    if (dst)
-                    {
-                        Point X((float)x, (float)y, dst.value()-0.02f);
-                        Vector3 I = r.GetDirection();
-                        Vector3 N = X.GetPos() - spheres[i].GetCenter();
-                        N = N.normalize();
-
-                        Direction R((-I.dot(N) * N * 2 + I).normalize());
-
-                        Rayon r2(X, R);
-                        for (int j = 0; j < sphereMiror.size(); j++)
-                        {
-                            auto dst2 = raySphereIntersect(r2, spheres[i]);
-                            if (dst2)
-                            {
-                                
-                            }
-                        }
-                        
-                    }
-
-                }
-
-            }
-        }*/
 
 
     encodeOneStep(filename, image, width, height);
